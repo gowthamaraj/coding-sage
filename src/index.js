993635e32482456ef5a3c38793de4c2734c01bb8
gowthamaraj <gowthamaraj360@gmail.com>
2020-08-15 19:51:28 +0530

    making it unstrict

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
-  </React.StrictMode>,
+    <BrowserRouter><App /></BrowserRouter>,
   document.getElementById('root')
 );
